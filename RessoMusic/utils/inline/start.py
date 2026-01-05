eab0d4366d0123919afe6e116770d84ef607a101
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-05 16:40:59 +0530

    Update start.py

--- RessoMusic/utils/inline/start.py
+++ RessoMusic/utils/inline/start.py
@@ -27,16 +27,17 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-            InlineKeyboardButton("˹ϻʏ ʜᴏϻє˼", url="https://t.me/+rQqhGhdP7tRkMmM1"),
+            InlineKeyboardButton("˹ϻʏ ʜᴏϻє˼", url="https://t.me/Mixed_Vi3ez"),
         ],
         [
-            InlineKeyboardButton("˹ᴀɴɪʏᴀ ᴛᴜɴᴇs˼♪", url="https://yukiapp-steel.vercel.app/"),
+            InlineKeyboardButton("˹ᏦᎥᏇᎥ ᴛᴜɴᴇs˼♪", url="https://yukiapp-steel.vercel.app/"),
         ],
         [
-            InlineKeyboardButton("˹ ϻʏ ϻᴧsᴛєʀ ˼", url="http://T.me/THE_BOSS_JI"),
+            InlineKeyboardButton("˹ ϻʏ ϻᴧsᴛєʀ ˼", url="http://T.me/II_Mikey"),
             # InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
+
 
 
